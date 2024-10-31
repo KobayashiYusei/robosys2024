928a23734a80c98b8f6026e0a0edf487237d727b
Yusei Kobayashi <s23C1054AF@s.chibakoudai.jp>
2024-10-31 12:02:20 +0900

    Add command

--- test.bash
+++ test.bash
@@ -1,6 +1,11 @@
 #!/bin/bash
+ng (){
+	echo ${1}行目が違う。もう人生を諦めろ。
+	res=1
+}
+res=0
 a=小林
-[ "$a" = 小林 ]
-echo $?
-[ "a?" = 松本 ]
-echo $?
+[ "$a" = 小林 ] || ng "$LINENO"
+[ "a?" = 松本 ] || ng "$LINENO"
+
+exit $res
