3d98d60869f4e0690ac7d42939a3dd40b22603f7
Yusei Kobayashi <s23C1054AF@s.chibakoudai.jp>
2024-10-31 12:47:21 +0900

    Add command

--- test.bash
+++ test.bash
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -xv
 # SPDX-FileCopyrightText: 2024 Yusei Kobayashi
 # SPDX-License-Identifier: BSD-3-Clause
 ng (){
@@ -10,6 +10,15 @@ res=0
 
 out=$(seq 5 | ./plus)
 [ "$out" = 15.0 ] || ng "$LINENO"
+
+out=$(echo あ | ./plus)
+[ "$?" = 1 ] || ng "$LINENO"
+[ "${out}" = "" ] || ng "$LINENO"
+
+out=$(echo | ./plus)
+[ "$?" = 1 ] || ng "$LINENO"
+[ "${out}" = "" ] || ng "$LINENO"
+
 [ "$res" = 0 ] && echo OK
 exit $res
 
