3df82f2ce0f72f1a7c55bd1b7808153663551ec6
Yusei Kobayashi <s23C1054AF@s.chibakoudai.jp>
2024-10-31 12:32:02 +0900

    テストコード追加

--- test.bash
+++ test.bash
@@ -1,11 +1,15 @@
 #!/bin/bash
+# SPDX-FileCopyrightText: 2024 Yusei Kobayashi
+# SPDX-License-Identifier: BSD-3-Clause
 ng (){
 	echo うん、なるほどね。${1}行目が違う。未ロボの皆さんは優秀だって聞いたんですけど。
 	res=1
 }
-res=0
-a=小林
-[ "$a" = 小林 ] || ng "$LINENO"
-[ "$a" = 松本 ] || ng "$LINENO"
 
+res=0
+
+out=$(seq 5 | ./plus)
+[ "$out" = 15.0 ] || ng "$LINENO"
+[ "$res" = 0 ] && echo OK
 exit $res
+
